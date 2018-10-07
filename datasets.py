d85624f694ba551ef10755253c2c7ce839739547
dromosys <android.robot.factory@gmail.com>
2018-10-07 12:30:06 +1100

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,5 +1,17 @@
 from fastai.dataset import *
 
+class CustomDataset(FilesDataset):
+    def __init__(self, fnames, y, transform, path):
+        self.y=y
+        assert(len(fnames)==len(y))
+        super().__init__(fnames, transform, path)
+        
+    def get_x(self, i): 
+        return open_image(os.path.join(self.path,self.fnames[i]))
+    def get_y(self, i): 
+        return open_image(os.path.join(self.path,self.y[i]))
+    def get_c(self): return 0
+    
 class DepthDataset(Dataset):
     def __init__(self,ds,dpth_dict):
         self.dpth = dpth_dict
