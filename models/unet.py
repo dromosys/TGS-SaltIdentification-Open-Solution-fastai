4bbcece5ee11bcda0199b9919ede4fae2ce9dbb1
dromosys <android.robot.factory@gmail.com>
2018-10-07 12:21:49 +1100

    Update unet.py

--- models/unet.py
+++ models/unet.py
@@ -23,3 +23,23 @@ class Unet34(nn.Module):
     
     def close(self):
         for sf in self.sfs: sf.remove()
+            
+class SaveFeatures():
+    features=None
+    def __init__(self, m): self.hook = m.register_forward_hook(self.hook_fn)
+    def hook_fn(self, module, input, output): self.features = output
+    def remove(self): self.hook.remove()
+    
+class UnetBlock(nn.Module):
+    def __init__(self, up_in, x_in, n_out):
+        super().__init__()
+        up_out = x_out = n_out//2
+        self.x_conv  = nn.Conv2d(x_in,  x_out,  1)
+        self.tr_conv = nn.ConvTranspose2d(up_in, up_out, 2, stride=2)
+        self.bn = nn.BatchNorm2d(n_out)
+        
+    def forward(self, up_p, x_p):
+        up_p = self.tr_conv(up_p)
+        x_p = self.x_conv(x_p)
+        cat_p = torch.cat([up_p,x_p], dim=1)
+        return self.bn(F.relu(cat_p))
