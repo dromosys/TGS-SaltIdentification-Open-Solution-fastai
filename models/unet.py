d485c831424358287370cf56dd3c6704bcfcf81c
dromosys <android.robot.factory@gmail.com>
2018-10-07 11:59:37 +1100

    Update unet.py

--- models/unet.py
+++ models/unet.py
@@ -1,1 +1,22 @@
-
+class Unet34(nn.Module):
+    def __init__(self, rn):
+        super().__init__()
+        self.rn = rn
+        self.sfs = [SaveFeatures(rn[i]) for i in [2,4,5,6]]
+        self.up1 = UnetBlock(512,256,256)
+        self.up2 = UnetBlock(256,128,256)
+        self.up3 = UnetBlock(256,64,256)
+        self.up4 = UnetBlock(256,64,256)
+        self.up5 = nn.ConvTranspose2d(256, 1, 2, stride=2)
+        
+    def forward(self,x):
+        x = F.dropout(F.relu(self.rn(x)),0.2)
+        x = self.up1(x, self.sfs[3].features)
+        x = self.up2(x, self.sfs[2].features)
+        x = self.up3(x, self.sfs[1].features)
+        x = self.up4(x, self.sfs[0].features)
+        x = self.up5(x)
+        return x[:,0]
+    
+    def close(self):
+        for sf in self.sfs: sf.remove()
